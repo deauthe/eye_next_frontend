80195b54e9cdc1c80f29b1dfca29512e59baf40c
deauthe <deauth@ANURAGs-MacBook-Air.local>
2024-05-16 17:03:42 +0530

    initialized global types, fixed dashboard settings

--- src/components/Dashboard/designerSettingsForm/settingsFormTest.tsx
+++ src/components/Dashboard/designerSettingsForm/settingsFormTest.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
 	Sheet,
 	SheetClose,
@@ -9,123 +10,30 @@ import {
 	SheetTrigger,
 } from "@/components/ui/sheet";
 import FormContent from "./formContent";
-import { Switch } from "@/components/ui/switch";
 import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { IoMdSettings } from "react-icons/io";
-import SelectProductModal from "./SelectProductModal";
 import {
 	getPublicSettings,
 	setPublicSettings,
 } from "@/helpers/api/designerApi";
 import { Skeleton } from "@/components/ui/skeleton";
 
-interface Settings {
-	isPrivate: boolean;
-	showDesigns: boolean;
-	designIds: string[];
-	showFollowers: boolean;
-	showFullName: boolean;
-	showPhone: boolean;
-	showDescription: boolean;
-	showCoverPhoto: boolean;
-	showProfilePhoto: boolean;
-	socialMediaLink1: string;
-	socialMediaLink2: string;
-	portfolioLink1: string;
-	portfolioLink2: string;
-}
-
-const staticSettings: Settings = {
-	isPrivate: false,
-	showDesigns: true,
-	designIds: [],
-	showFollowers: true,
-	showFullName: true,
-	showPhone: true,
-	showDescription: true,
-	showCoverPhoto: true,
-	showProfilePhoto: true,
-	socialMediaLink1: "instagram.com",
-	socialMediaLink2: "alshndlakwnd.com",
-	portfolioLink1: "ahha",
-	portfolioLink2: "akljshdlkasd",
-};
-
 const SettingsSheet = () => {
 	const designerId = "656f24446f2ab5347da947bd";
-	const [newSettings, setNewSettings] = useState(null);
-	const [portfolioLinks, setPortfolioLinks] = useState({
-		link1: "",
-		link2: "",
-	});
-	const [socialMediaLinks, setSocialMediaLinks] = useState({
-		link1: "",
-		link2: "",
-	});
 	const [loading, setLoading] = useState(true);
-
 	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				let data;
-				if (designerId) {
-					data = await getPublicSettings(designerId);
-					setNewSettings(data);
-					setPortfolioLinks({
-						link1: data.settings.portfolioLinks[0]
-							? data.settings.portfolioLinks[0]
-							: "",
-						link2: data.settings.portfolioLinks[1]
-							? data.settings.portfolioLinks[1]
-							: "",
-					});
-					setLoading(false);
-					console.log("settings data:", data);
-				}
-			} catch (error) {
-				console.error("Error fetching designer Data, please check Id:", error);
-			} finally {
-				setLoading(false);
-			}
-		};
-		if (!newSettings) {
-			fetchData();
-		}
-		console.log(newSettings);
+		setTimeout(() => {
+			setLoading(false);
+		}, 2000);
 	}, []);
-	// const settings = usePublicSettings();
 
-	const setLinks = () => {
-		setNewSettings({
-			...newSettings,
-			settings: {
-				...newSettings.settings,
-				socialMedia: [socialMediaLinks.link1, socialMediaLinks.link2],
-				portfolioLinks: [portfolioLinks.link1, portfolioLinks.link2],
-			},
-		});
-	};
-
-	const handleSubmit = async () => {
+	const handleSubmit = async (settings: any) => {
 		setLoading(true);
-
-		// Update the state with setLinks
-		setLinks();
-
-		console.log("settings from handle submit: ", newSettings);
-
-		// Perform the API call
 		await setPublicSettings({
-			settings: newSettings,
+			settings: settings,
 			designer_id: designerId,
 		});
-
-		const updatedSettings = await getPublicSettings(designerId);
-		console.log("updatedSettings", updatedSettings);
-
 		setLoading(false);
 	};
 
@@ -139,9 +47,9 @@ const SettingsSheet = () => {
 					</Button>
 				</SheetTrigger>
 
-				<SheetContent>
+				<SheetContent className="bg-black/[0.8] text-white/[0.6] overflow-y-auto">
 					<SheetHeader>
-						<SheetTitle>Edit profile</SheetTitle>
+						<SheetTitle className="text-white/[.3]">Edit profile</SheetTitle>
 						<SheetDescription>
 							{"Make changes to your profile here. Click save when youre done."}
 						</SheetDescription>
@@ -153,17 +61,14 @@ const SettingsSheet = () => {
 									key={index}
 									className="rounded-md h-40 flex flex-col gap-5 my-4 "
 								>
-									<Skeleton className="w-full h-1/6 rounded-md shadow-md my-5 bg-black/[0.2]" />
-									<Skeleton className="w-full h-1/3 rounded-md shadow-lg my-5" />
+									<Skeleton className="w-full h-1/6 rounded-md shadow-md my-5 bg-primary/[0.2]" />
+									<Skeleton className="w-full h-1/3 rounded-md shadow-lg my-5 bg-white/[0.2]" />
 								</div>
 							);
 						})
 					) : (
 						<div className="">
-							<FormContent
-								defaultValues={newSettings?.settings}
-								designerId={designerId}
-							/>
+							<FormContent onSubmit={handleSubmit} designerId={designerId} />
 						</div>
 					)}
 				</SheetContent>
