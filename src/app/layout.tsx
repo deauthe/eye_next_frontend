e767115abf0a249c45cff2dfe672d80e109e354f
Abhshek Kumar <abui8994ty@gmail.com>
2024-05-18 14:08:31 +0530

    added cart.jsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Providers } from "./store/provider";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
