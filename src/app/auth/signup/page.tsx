4391dc781b32d257a2c8eb50d5e41802e798e297
deauthe <deauth@ANURAGs-MacBook-Air.local>
2024-05-18 18:02:14 +0530

    added login and signIn page

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,210 +1,63 @@
 "use client";
-import React, { useState } from "react";
-import { BsCheckCircleFill } from "react-icons/bs";
-import logoLight from "../../../../public/logo.png";
+import SignupForm from "@/components/auth/SignUpForm";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import Image from "next/image";
-import { FcGoogle } from "react-icons/fc";
-import Wrapper from "@/components/Wrapper";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useRouter } from "next/navigation";
-import SignupCover from "@/components/auth/SignupCover";
+import React, { useState } from "react";
 
-const SignUp = () => {
-	const router = useRouter();
+type Props = {};
 
-	const redirectToLoginWithDelay = () => {
-		setTimeout(() => {
-			router.push("./login");
-		}, 3000); // Delay for 2 seconds (2000 milliseconds)
-	};
-
-	const toastify = (message: string, res: string) => {
-		if (res) {
-			toast.success(message);
-		} else {
-			toast.error(message);
-		}
-	};
-
-	// ============= Initial State Start here =============
-	const [clientName, setClientName] = useState("");
-	const [email, setEmail] = useState("");
-
-	console.log(clientName, email);
-	// ============= Initial State End here ===============
-	// ============= Error Msg Start here =================
-	const [errClientName, setErrClientName] = useState("");
-	const [errEmail, setErrEmail] = useState("");
-	// ============= Error Msg End here ===================
-	const [successMsg, setSuccessMsg] = useState("");
-	// ============= Event Handler Start here =============
-	const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setClientName(e.target.value);
-		setErrClientName("");
-	};
-	const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setEmail(e.target.value);
-		setErrEmail("");
-	};
-
-	// ============= Event Handler End here ===============
-	// ================= Email Validation start here =============
-	const EmailValidation = (email: string) => {
-		return String(email)
-			.toLowerCase()
-			.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
-	};
-	// ================= Email Validation End here ===============
-
-	const handleSignUp = async () => {
-		const apiUrl = "https://eye-eye-tee.onrender.com/api/user/create";
-
-		const requestBody = JSON.stringify({
-			username: clientName,
-			email: email,
-		});
-
-		console.log(requestBody);
-
-		const headers = {
-			"Content-Type": "application/json",
-			"x-api-key": "token",
-		};
-
-		try {
-			const response = await fetch(apiUrl, {
-				method: "POST",
-				headers: headers,
-				body: requestBody,
-			});
-
-			if (response.ok) {
-				const responseData = await response.json();
-				toastify(responseData.message, response.ok ? "true" : "false");
-				// loading bar
-				redirectToLoginWithDelay();
-			} else {
-				console.error("API call failed with status:", response.status);
-				return null;
-			}
-		} catch (error) {
-			console.error("An error occurred while making the API call:", error);
-			return null;
-		}
-	};
-
+const Signup = (props: Props) => {
+	const [signupType, setSignUpType] = useState<"artist" | "customer">(
+		"customer"
+	);
 	return (
-		<Wrapper>
-			<div className="w-full h-screen flex items-center justify-start my-5">
-				<SignupCover />
-				<div className="w-full lgl:w-[500px] h-full flex flex-col justify-center">
-					{false ? (
-						<div className="w-[500px]">
-							<p className="w-full px-4 py-10 text-green-500 font-medium font-titleFont">
-								{successMsg}
-							</p>
-							<Link href={"/auth/login"}>
-								<button
-									className="w-full h-10 bg-primeColor rounded-md text-base font-titleFont font-semibold 
-            tracking-wide hover:bg-black border-2 border-black hover:text-white duration-300"
-								>
-									Sign in
-								</button>
-							</Link>
-						</div>
-					) : (
-						<div className="w-full  h-screen flex items-center justify-center mt-44">
-							<div className="px-6 py-4 w-[500px] h-[96%] flex flex-col justify-start overflow-y-scroll scrollbar-thin scrollbar-thumb-primeColor">
-								<h1 className="font-titleFont underline underline-offset-4 decoration-[1px] font-semibold text-2xl mdl:text-3xl mb-4">
-									Create your account
-								</h1>
-								<div className="flex flex-col gap-3">
-									{/* client name */}
-									<div className="flex flex-col gap-.5">
-										<p className="font-titleFont text-base font-semibold text-gray-600">
-											User Name
-										</p>
-										<input
-											onChange={handleName}
-											value={clientName}
-											className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
-											type="text"
-											placeholder="eg. John Doe"
-										/>
-										{errClientName && (
-											<p className="text-sm text-red-500 font-titleFont font-semibold px-4">
-												<span className="font-bold italic mr-1">!</span>
-												{errClientName}
-											</p>
-										)}
-									</div>
-									{/* Email */}
-									<div className="flex flex-col gap-.5">
-										<p className="font-titleFont text-base font-semibold text-gray-600">
-											Work Email
-										</p>
-										<input
-											onChange={handleEmail}
-											value={email}
-											className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
-											type="email"
-											placeholder="john@workemail.com"
-										/>
-										{errEmail && (
-											<p className="text-sm text-red-500 font-titleFont font-semibold px-4">
-												<span className="font-bold italic mr-1">!</span>
-												{errEmail}
-											</p>
-										)}
-									</div>
-									{/* Phone Number */}
-
-									<div className="flex justify-center items-center w-full">
-										<button
-											onClick={handleSignUp}
-											className={`
-                      checked
-                       "bg-primeColor hover:bg-black hover:text-white text-black cursor-pointer"
-                      
-                     w-[59%] text-black  text-base font-medium h-10 border-2 border-black   rounded-full hover:text-white hover:bg-black duration-300`}
-										>
-											Create Account
-										</button>
-									</div>
-									<p className="text-sm text-center font-titleFont font-medium">
-										{" "}
-										Dont have an Account?
-										<Link href={"/auth/login"}>
-											<span className="hover:text-blue-600 duration-300">
-												Sign in
-											</span>
-										</Link>
-									</p>
-
-									<div className="text-center w-full ">
-										<p>or</p>
-									</div>
-
-									<div className="flex justify-center items-center  ">
-										<span className="text-2xl border-2 border-r-0 border-black p-1 pl-5 rounded-l-full">
-											<FcGoogle />
-										</span>
-										<button className=" text-md font-[550] gap-2 border-2 border-black border-l-0 pl-1 p-1 pr-5 rounded-r-full">
-											Continue with google{" "}
-										</button>
-									</div>
-								</div>
-							</div>
-						</div>
-					)}
-
-					<ToastContainer />
+		<div className="flex flex-col w-full min-h-screen h-fit bg-white">
+			<div className="md:w-2/3 rounded-3xl flex flex-col bg-black/80 mx-auto md:py-10 gap-10">
+				<div className="mx-auto w-2/3 flex flex-col gap-3">
+					<h1 className="mx-auto w-fit text-5xl text-white font-heading1">
+						join deauth
+					</h1>
+					<h2 className="mx-auto w-1/2 font-extralight text-sm text-white text-center">
+						sign up as customer for 20% off on yours first order. coupons will
+						be directly sent to the email registered
+					</h2>
+					<Link
+						href="auth/login"
+						className="text-md  text-white mx-auto px-6 py-0 rounded-full hover:bg-accent border-[1px] border-white/20"
+					>
+						Sign In Instead
+					</Link>
 				</div>
+				{/* signup/ login option  */}
+				<div className="w-5/6 md:grid md:grid-cols-2 flex flex-col items-center md:gap-10 mx-auto">
+					<Button
+						className={`md:w-full w-1/2 border-[1px] border-white/80 flex flex-col items-center text-center md:h-20 bg-secondaryBackground md:py-16 ${
+							signupType === "artist" && "bg-accent"
+						}`}
+						onClick={() => setSignUpType("artist")}
+					>
+						<h1 className="text-3xl font-heading1 ">artists</h1>
+						<h2 className="font-extralight w-1/2 text-wrap">
+							set up your shop and start selling your design
+						</h2>
+					</Button>
+					<Button
+						className={`md:w-full w-1/2 border-[1px] border-white/80 flex flex-col items-center text-center md:h-20 bg-secondaryBackground md:py-16 ${
+							signupType === "customer" && "bg-accent"
+						}`}
+						onClick={() => setSignUpType("customer")}
+					>
+						<h1 className="text-3xl font-heading1 ">customer</h1>
+						<h2 className="font-extralight w-1/2 text-wrap">
+							explore the marketplace and find products to buy
+						</h2>
+					</Button>
+				</div>
+				{/* conditional render for forms */}
+				{signupType === "artist" ? <SignupForm /> : <SignupForm />}
 			</div>
-		</Wrapper>
+		</div>
 	);
 };
 
-export default SignUp;
+export default Signup;
---
+++ src/components/auth/LoginForm.tsx
@@ -0,0 +1,162 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { ToastContainer, toast } from "react-toastify";
+import { FcGoogle } from "react-icons/fc";
+import "react-toastify/dist/ReactToastify.css";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
+import axios, { AxiosRequestConfig } from "axios";
+import { ToastAction } from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
+
+const LoginForm = () => {
+	const { toast } = useToast();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	const router = useRouter();
+
+	const redirectToLoginWithDelay = () => {
+		setTimeout(() => {
+			router.push("./login");
+		}, 3000); // Delay for 2 seconds (2000 milliseconds)
+	};
+
+	const toastify = (message: string, res: string) => {
+		toast({
+			title: message,
+			description: res,
+		});
+	};
+
+	const onSubmit = async (data: any) => {
+		const apiUrl = "http://localhost:8080/api/user/login";
+		const headers = {
+			"Content-Type": "application/json",
+			"x-api-key": "token",
+		};
+		const requestBody = {
+			email: data.email,
+			password: data.password,
+		};
+		const payload: AxiosRequestConfig = {
+			method: "post",
+			headers,
+			url: apiUrl,
+			data: requestBody,
+		};
+		try {
+			let response = await axios(payload);
+
+			if (response.status == 200) {
+				toastify(response.statusText, response.status ? "true" : "false");
+				// loading bar
+				redirectToLoginWithDelay();
+			} else {
+				console.error("API call failed with status:", response.status);
+				return null;
+			}
+		} catch (error) {
+			console.error("An error occurred while making the API call:", error);
+			return null;
+		}
+		// Handle form submission
+		console.log(data);
+	};
+	const handleGoogleAuth = async () => {
+		const apiUrl = "http://localhost:8080/api/auth/google";
+
+		window.open(apiUrl, "_self");
+
+		// if (authWindow) {
+		//   // Polling to check if the window is closed
+		//   const checkClosed = async () => {
+		//     if (authWindow.closed) {
+		//       console.log("Authentication window closed");
+		//       // After the window is closed, fetch user data
+		//       await fetchUser();
+		//     } else {
+		//       // Continue polling until the window is closed
+		//       setTimeout(checkClosed, 1000);
+		//     }
+		//   };
+
+		//   // Start polling
+		//   checkClosed();
+		// }
+
+		// console.log("hereWindow", newWindow);
+
+		// if (newWindow) {
+		//   timer = setInterval(async () => {
+		//     if (newWindow.closed) {
+		//       console.log("we are authenticated");
+		//       await fetchUser();
+		//       if (timer) clearInterval(timer);
+		//     }
+		//   }, 2500);
+		// }
+	};
+
+	return (
+		<div className="w-full h-full  flex flex-col justify-center mx-auto">
+			<div className="px-6 py-4 w-[500px] flex flex-col mx-auto ">
+				<form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
+					{/* Email */}
+					<div className="flex flex-col gap-0.5">
+						<Input
+							{...register("email", { required: "Email is required" })}
+							className="w-full text-white h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-none border-b-2 border-white/70 focus:border-none outline-none placeholder:text-white/70"
+							type="email"
+							placeholder="Email"
+						/>
+						{errors.email && (
+							<p className="text-sm text-red-500 font-titleFont font-semibold px-4">
+								<span className="font-bold italic mr-1">!</span>
+								{errors.email.message as string}
+							</p>
+						)}
+					</div>
+					{/* password */}
+					<div className="flex flex-col gap-0.5">
+						<Input
+							{...register("password", { required: "password is required" })}
+							className="w-full h-8 text-white placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-none border-b-2 border-white/70 focus:border-none outline-none placeholder:text-white/70"
+							type="password"
+							placeholder="Password"
+						/>
+						{errors.password && (
+							<p className="text-sm text-red-500 font-titleFont font-semibold px-4">
+								<span className="font-bold italic mr-1">!</span>
+								{errors.password.message as string}
+							</p>
+						)}
+					</div>
+
+					<div className="flex justify-center items-center w-full">
+						<Button type="submit" className="rounded-full px-10 bg-black/60">
+							Sign Up
+						</Button>
+					</div>
+
+					<div className="flex justify-center items-center">
+						<Button
+							onClick={handleGoogleAuth}
+							className="rounded-full px-10 bg-black/60"
+						>
+							<span className="text-2xl ">
+								<FcGoogle />
+							</span>
+							<span>Continue with Google</span>
+						</Button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default LoginForm;
